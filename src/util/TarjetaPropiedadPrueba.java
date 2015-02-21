fe862bf5d110f98ab696d1ca08c8e88cd8f122d6
Cesar Estrada <cesarestrada090@gmail.com>
2015-02-21 17:35:36 -0500

    equipos y codigos migrados

---
+++ src/util/TarjetaPropiedadPrueba.java
@@ -0,0 +1,41 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package util;
+
+import DATOS.CertificadoDAL;
+import DATOS.ObservacionDAL;
+import ENTIDADES.Certificado;
+import ENTIDADES.Observacion;
+import java.util.Date;
+
+/**
+ *
+ * @author rchuc
+ */
+public class TarjetaPropiedadPrueba {
+
+    public static void main(String[] args) {
+        Certificado c = new Certificado();
+        c.setTipoDocTransp("1");// 1 RUC 2 DNI 3 Carnet Extran 
+        c.setClaseAutorizacion("M1");
+        c.setTipoDocEvaluar("1");// 1 CITV, 2 Inspeccion tecnica
+        c.setNumDocTransp("46541974"); 
+        c.setCodLocal("01"); // 01 central, 02 planta movil
+        c.setcIdentidadCert("00");// Aún no se tiene el codigo de la entidad, debe darlo el MTC
+        c.setNumDocEvaluar("A651154");
+        c.setRazonTransp("Empresa Orion");
+        c.setResultado(1);
+        c.setVigencia("12"); //meses
+        c.setUbigeo("130100");  // codigo de Truj en el INEI
+        Date d= new Date();
+        c.setFecInspeccion(d);
+        c.setFecVencimiento(d);
+        
+        
+        
+        CertificadoDAL data= new CertificadoDAL();
+        System.out.print(data.registrarDatosCertificado(c)) ;
+    }
+}
